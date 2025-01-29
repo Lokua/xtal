7d7b81d81b5de33e518205200e5cd619840025e7
Lokua <dev@lokua.net>
2025-01-29 17:46:14 -0600

    feat: control_script POC fin

--- src/framework/control_script.rs
+++ src/framework/control_script.rs
@@ -1,5 +1,12 @@
+use notify::{Event, RecursiveMode, Watcher};
 use serde::Deserialize;
-use std::{collections::HashMap, error::Error, fs, path::PathBuf};
+use std::{
+    collections::HashMap,
+    error::Error,
+    fs,
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 use super::prelude::*;
 
@@ -31,24 +38,36 @@ enum ControlType {
 
 type ConfigFile = HashMap<String, MaybeControlConfig>;
 
+struct UpdateState {
+    _watcher: notify::RecommendedWatcher,
+    state: Arc<Mutex<Option<ConfigFile>>>,
+}
+
 pub struct ControlScript<T: TimingSource> {
-    /// The raw yaml representation
+    /// The raw yaml representation that represents the last valid parsed state
     control_configs: Option<ConfigFile>,
     pub controls: Controls,
     osc_controls: OscControls,
     animation: Animation<T>,
     keyframe_sequences: HashMap<String, (LerpAbsConfig, Vec<Keyframe>)>,
-    // _watcher: notify::RecommendedWatcher,
+    update_state: UpdateState,
 }
 
 impl<T: TimingSource> ControlScript<T> {
     pub fn new(path: PathBuf, timing: T) -> Self {
+        let state = Arc::new(Mutex::new(None));
+        let state_clone = state.clone();
+
         let mut script = Self {
             control_configs: None,
             controls: Controls::with_previous(vec![]),
             osc_controls: OscControls::new(),
             animation: Animation::new(timing),
             keyframe_sequences: HashMap::new(),
+            update_state: UpdateState {
+                state: state.clone(),
+                _watcher: Self::setup_watcher(path.clone(), state_clone),
+            },
         };
         script
             .import_script(&path)
@@ -56,12 +75,65 @@ impl<T: TimingSource> ControlScript<T> {
         script
     }
 
+    pub fn get(&self, name: &str) -> f32 {
+        if name.starts_with("/") {
+            return self.osc_controls.get(name);
+        }
+
+        if self.controls.has(name) {
+            return self.controls.float(name);
+        }
+
+        if self.keyframe_sequences.contains_key(name) {
+            let (config, keyframes) =
+                self.keyframe_sequences.get(name).unwrap();
+            return self.animation.lerp(keyframes.clone(), config.delay);
+        }
+
+        error!("No control named {}", name);
+        panic!()
+    }
+
+    pub fn update(&mut self) {
+        let new_config = {
+            if let Ok(mut guard) = self.update_state.state.lock() {
+                guard.take()
+            } else {
+                None
+            }
+        };
+
+        if let Some(config) = new_config {
+            if let Err(e) = self.apply_config(config) {
+                error!("Failed to apply new configuration: {:?}", e);
+            }
+        }
+    }
+
     fn import_script(&mut self, path: &PathBuf) -> Result<(), Box<dyn Error>> {
-        let file_content = fs::read_to_string(&path)?;
-        let control_configs: ConfigFile = serde_yml::from_str(&file_content)?;
-        self.control_configs = Some(control_configs);
+        let config = Self::parse_config(path)?;
+        self.apply_config(config)
+    }
+
+    fn parse_config(path: &PathBuf) -> Result<ConfigFile, Box<dyn Error>> {
+        let file_content = fs::read_to_string(path)?;
+        let config = serde_yml::from_str(&file_content)?;
+        Ok(config)
+    }
+
+    fn apply_config(
+        &mut self,
+        config: ConfigFile,
+    ) -> Result<(), Box<dyn Error>> {
+        self.control_configs = Some(config);
+
+        self.controls = Controls::with_previous(vec![]);
+        self.osc_controls = OscControls::new();
+        self.keyframe_sequences.clear();
+
         self.populate_controls()?;
         self.osc_controls.start()?;
+
         Ok(())
     }
 
@@ -141,27 +213,47 @@ impl<T: TimingSource> ControlScript<T> {
         Ok(())
     }
 
-    pub fn get(&self, name: &str) -> f32 {
-        if name.starts_with("/") {
-            return self.osc_controls.get(name);
-        }
+    fn setup_watcher(
+        path: PathBuf,
+        state: Arc<Mutex<Option<ConfigFile>>>,
+    ) -> notify::RecommendedWatcher {
+        let path_to_watch = path.clone();
 
-        if self.controls.has(name) {
-            return self.controls.float(name);
-        }
+        let mut watcher = notify::recommended_watcher(move |res| {
+            let event: Event = match res {
+                Ok(event) => event,
+                Err(_) => return,
+            };
 
-        if self.keyframe_sequences.contains_key(name) {
-            let (config, keyframes) =
-                self.keyframe_sequences.get(name).unwrap();
-            return self.animation.lerp(keyframes.clone(), config.delay);
-        }
+            if event.kind
+                != notify::EventKind::Modify(notify::event::ModifyKind::Data(
+                    notify::event::DataChange::Content,
+                ))
+            {
+                return;
+            }
 
-        error!("No control named {}", name);
-        panic!()
-    }
+            info!("{:?} changed. Attempting to reload configuration.", path);
 
-    pub fn update(&self) {
-        //
+            match Self::parse_config(&path) {
+                Ok(new_config) => {
+                    if let Ok(mut guard) = state.lock() {
+                        info!("Loaded new configuration.");
+                        *guard = Some(new_config);
+                    }
+                }
+                Err(e) => {
+                    error!("Failed to load updated configuration: {:?}", e)
+                }
+            }
+        })
+        .expect("Failed to create watcher");
+
+        watcher
+            .watch(&path_to_watch, RecursiveMode::NonRecursive)
+            .expect("Failed to start watching file");
+
+        watcher
     }
 }
 
