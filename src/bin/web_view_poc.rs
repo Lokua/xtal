71b4983a915571f68b983487453e5eb7afb5988e
Lokua <dev@lokua.net>
2025-03-25 18:53:08 -0500

    feat(wv): derive window height, set constraints

--- src/bin/web_view_poc.rs
+++ src/bin/web_view_poc.rs
@@ -1,13 +1,22 @@
 use ipc_channel::ipc::{self, IpcSender};
 use tao::{
-    dpi::{LogicalPosition, LogicalSize},
+    dpi::{self, LogicalPosition, LogicalSize, PixelUnit},
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::{Theme, WindowBuilder},
+    window::{Theme, WindowBuilder, WindowSizeConstraints},
 };
 use wry::WebViewBuilder;
 
-use lattice::{framework::prelude::*, runtime::web_view as wv};
+use lattice::{
+    framework::prelude::*,
+    runtime::web_view::{self as wv, SerializableControl},
+};
+
+const DEFAULT_WIDTH: i32 = 560;
+const DEFAULT_HEIGHT: i32 = 700;
+// Eyeballed from devtools. TODO: parse the variables from the CSS file?
+const HEADER_HEIGHT: i32 = 70;
+const FOOTER_HEIGHT: i32 = 81 + 27;
 
 fn main() -> wry::Result<()> {
     init_logger();
@@ -22,8 +31,22 @@ fn main() -> wry::Result<()> {
     let window = WindowBuilder::new()
         .with_title("GUI")
         .with_theme(Some(ternary!(is_light, Theme::Light, Theme::Dark)))
-        .with_inner_size(LogicalSize::new(560, 700))
+        .with_inner_size(LogicalSize::new(DEFAULT_WIDTH, DEFAULT_HEIGHT))
+        // TODO: set x offset based on actual sketch width (but not in
+        // performance mode!)
         .with_position(LogicalPosition::new(700, 0))
+        .with_inner_size_constraints(WindowSizeConstraints {
+            min_width: Some(PixelUnit::Logical(dpi::LogicalUnit(
+                DEFAULT_WIDTH as f64,
+            ))),
+            min_height: Some(PixelUnit::Logical(dpi::LogicalUnit(
+                (HEADER_HEIGHT + FOOTER_HEIGHT + 64) as f64,
+            ))),
+            max_width: Some(PixelUnit::Logical(dpi::LogicalUnit(
+                DEFAULT_WIDTH as f64 + 64.0,
+            ))),
+            max_height: None,
+        })
         .build(&event_loop)
         .unwrap();
 
@@ -59,6 +82,7 @@ fn main() -> wry::Result<()> {
         match receiver.try_recv() {
             Ok(event) => {
                 trace!("Child: Received parent event: {:?}", event);
+
                 let script = format!(
                     "window.postMessage({}, '*');",
                     serde_json::to_string(&event).unwrap()
@@ -67,11 +91,20 @@ fn main() -> wry::Result<()> {
                     error!("Failed to send data to WebView: {:?}", e);
                 }
 
+                // allowing as likely to add more matches in the future
                 #[allow(clippy::single_match)]
                 match event {
-                    wv::Event::LoadSketch { display_name, .. } => {
-                        debug!("Received LoadSketch. Attempting to set title");
+                    wv::Event::LoadSketch {
+                        display_name,
+                        controls,
+                        ..
+                    } => {
+                        debug!("Received LoadSketch. Setting title and height");
                         window.set_title(&format!("{} Controls", display_name));
+                        window.set_inner_size(LogicalSize::new(
+                            DEFAULT_WIDTH,
+                            derive_gui_height(controls),
+                        ));
                     }
                     _ => {}
                 }
@@ -101,4 +134,23 @@ fn setup_ipc_connection(
     let bootstrap = IpcSender::connect(server_name)?;
     bootstrap.send((to_child, from_child))?;
     Ok((to_parent, from_parent))
+}
+
+fn derive_gui_height(controls: Vec<SerializableControl>) -> i32 {
+    let controls_height: i32 = controls
+        .iter()
+        .map(|c| match c {
+            SerializableControl::DynamicSeparator { .. }
+            | SerializableControl::Separator {} => 9,
+            _ => 24,
+        })
+        .sum();
+    let non_scientific_offset = controls.len() as i32;
+
+    let h =
+        HEADER_HEIGHT + controls_height + FOOTER_HEIGHT + non_scientific_offset;
+
+    debug!("Derived GUI height: {}", h);
+
+    h
 }
