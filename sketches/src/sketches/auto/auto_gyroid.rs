d514e84cb43ae9a43bce22ea1f4df8d7f1b97125
Lokua <dev@lokua.net>
2026-02-15 13:35:45 -0600

    feat(auto_gyroid): pre-optim

--- sketches/src/sketches/auto/auto_gyroid.rs
+++ sketches/src/sketches/auto/auto_gyroid.rs
@@ -16,7 +16,8 @@ pub const SKETCH_CONFIG: SketchConfig = SketchConfig {
 #[derive(SketchComponents)]
 pub struct AutoGyroid {
     hub: ControlHub<Timing>,
-    gpu: gpu::GpuState<gpu::BasicPositionVertex>,
+    shader_1: gpu::GpuState<gpu::BasicPositionVertex>,
+    shader_2: gpu::GpuState<gpu::BasicPositionVertex>,
 }
 
 #[uniforms(banks = 12)]
@@ -32,7 +33,7 @@ pub fn init(app: &App, ctx: &Context) -> AutoGyroid {
 
     let params = ShaderParams::default();
 
-    let gpu = gpu::GpuState::new_fullscreen(
+    let shader_1 = gpu::GpuState::new_fullscreen(
         app,
         wr.resolution_u32(),
         to_absolute_path(file!(), "auto_gyroid.wgsl"),
@@ -40,7 +41,19 @@ pub fn init(app: &App, ctx: &Context) -> AutoGyroid {
         0,
     );
 
-    AutoGyroid { hub, gpu }
+    let shader_2 = gpu::GpuState::new_fullscreen(
+        app,
+        wr.resolution_u32(),
+        to_absolute_path(file!(), "auto_gyroid_post.wgsl"),
+        &params,
+        1,
+    );
+
+    AutoGyroid {
+        hub,
+        shader_1,
+        shader_2,
+    }
 }
 
 impl Sketch for AutoGyroid {
@@ -48,10 +61,16 @@ impl Sketch for AutoGyroid {
         let wr = ctx.window_rect();
         let mut params = ShaderParams::from((&wr, &self.hub));
         params.set("a3", self.hub.animation.beats());
-        self.gpu.update_params(app, wr.resolution_u32(), &params);
+        let res = wr.resolution_u32();
+
+        self.shader_1.update_params(app, res, &params);
+        self.shader_2.update_params(app, res, &params);
+
+        let texture = self.shader_1.render_to_texture(app);
+        self.shader_2.set_texture(app, &texture);
     }
 
     fn view(&self, _app: &App, frame: Frame, _ctx: &Context) {
-        self.gpu.render(&frame);
+        self.shader_2.render(&frame);
     }
 }
