7c192a3815139859e0aa6021aa5ac32c886e54ab
Lokua <dev@lokua.net>
2026-05-14 08:08:23 -0500

    fix: user data dir

--- sketches/src/constants.rs
+++ sketches/src/constants.rs
@@ -2,3 +2,8 @@
 pub const HD_WIDTH: u32 = 1920 / 2;
 #[allow(dead_code)]
 pub const HD_HEIGHT: u32 = 1080 / 2;
+
+#[allow(dead_code)]
+pub const IG_WIDTH: u32 = 1080 / 2;
+#[allow(dead_code)]
+pub const IG_HEIGHT: u32 = 1920 / 2;
